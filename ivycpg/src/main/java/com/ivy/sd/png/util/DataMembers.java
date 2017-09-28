fba939e96e5516418610a23cf01412efda76fa95
Anish K <anish.k@ivymobility.com>
2017-09-28 17:45:33 +0530

    Ack_Date- ActDate

--- ivycpg/src/main/java/com/ivy/sd/png/util/DataMembers.java
+++ ivycpg/src/main/java/com/ivy/sd/png/util/DataMembers.java
@@ -585,8 +585,8 @@ public class DataMembers {
     public static final String tbl_ModuleActivityDetails_cols = "Tid,DistributorId,UserId,Date,ModuleCode,Activity,Latitude,longitude,GpsAccuracy";
 
     public static final String tbl_jointcallacknowledgement = "JointCallAcknowledgement";
-    public static final String tbl_jointcallacknowledgement_cols = "Userid,Username,Beat,Retailer,Date,Value,Refid,Ack_Date,Upload";
-    public static final String tbl_jointcallacknowledgement_upload_cols = "Refid,Ack_Date";
+    public static final String tbl_jointcallacknowledgement_cols = "Userid,Username,Beat,Retailer,Date,Value,Refid,AckDate,Upload";
+    public static final String tbl_jointcallacknowledgement_upload_cols = "Refid,AckDate";
 
     private static final String tbl_OrderDeliveryDetail = "OrderDeliveryDetail";
     private static final String tbl_OrderDeliveryDetail_cols = "orderId,productId,uomId,qty,uomCount,price,taxPrice,lineValue";
