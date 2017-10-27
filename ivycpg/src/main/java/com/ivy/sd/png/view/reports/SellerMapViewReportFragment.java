2ca3dc5b44ba824d0997c843fa7bd6edad011efd
rajkumar.s <rajkumar.s@ivymobility.com>
2017-10-27 21:16:47 +0530

    seller mapview partial commit

---
+++ ivycpg/src/main/java/com/ivy/sd/png/view/reports/SellerMapViewReportFragment.java
@@ -0,0 +1,46 @@
+package com.ivy.sd.png.view.reports;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+import com.ivy.sd.png.asean.view.R;
+import com.ivy.sd.png.model.BusinessModel;
+
+/**
+ * Created by rajkumar.s on 10/27/2017.
+ */
+
+public class SellerMapViewReportFragment extends Fragment {
+
+    View view;
+    BusinessModel bmodel;
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        getActivity().getWindow().setSoftInputMode(
+                WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+
+        view = inflater.inflate(R.layout.fragment_order_report, container, false);
+
+        bmodel = (BusinessModel) getActivity().getApplicationContext();
+        bmodel.setContext(getActivity());
+
+        if (bmodel.userMasterHelper.getUserMasterBO().getUserid() == 0) {
+            Toast.makeText(getActivity(),
+                    getResources().getString(R.string.sessionout_loginagain),
+                    Toast.LENGTH_SHORT).show();
+            getActivity().finish();
+        }
+
+
+        return view;
+    }
+
+
+    }
