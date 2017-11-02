c7a700e89105a642da54206aeaf772046f068cc4
mansoor.k <mansoor.k@ivymobility.com>
2017-11-02 15:14:38 +0530

    Added Row Layout for SellerPerformance Report

--- ivycpg/src/main/java/com/ivy/sd/png/view/reports/SellerPerformanceReportFragment.java
+++ ivycpg/src/main/java/com/ivy/sd/png/view/reports/SellerPerformanceReportFragment.java
@@ -13,18 +13,25 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.ivy.sd.png.asean.view.R;
 import com.ivy.sd.png.bo.OutletReportBO;
 import com.ivy.sd.png.bo.SellerPerformanceBO;
 import com.ivy.sd.png.commons.IvyBaseFragment;
+import com.ivy.sd.png.commons.SDUtil;
 import com.ivy.sd.png.model.BusinessModel;
+import com.ivy.sd.png.provider.ConfigurationMasterHelper;
 import com.ivy.sd.png.util.Commons;
 import com.ivy.sd.png.util.DataMembers;
 import com.ivy.sd.png.view.HomeScreenActivity;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by mansoor.k on 11/01/2017.
@@ -114,6 +121,8 @@ public class SellerPerformanceReportFragment extends IvyBaseFragment {
                 for (OutletReportBO user : userList) {
                     int plannedCall = 0, deviateCall = 0, actualCall = 0, productiveCall = 0;
                     double vistPer = 0, productivePer = 0, actual = 0, objective = 0;
+                    long duration = 0;
+                    String lastSync = "";
                     SellerPerformanceBO sellerPerformanceBO = new SellerPerformanceBO();
                     sellerPerformanceBO.setUserId(user.getUserId());
                     sellerPerformanceBO.setUserName(user.getUserName());
@@ -129,6 +138,17 @@ public class SellerPerformanceReportFragment extends IvyBaseFragment {
                                 actualCall += 1;
                             if (outletReport.getIsVisited().equalsIgnoreCase("Y") && Double.parseDouble(outletReport.getSalesValue()) > 0)
                                 productiveCall += 1;
+
+                            //parse date and sum up intervals
+                            duration += getDiffDuration(outletReport.getTimeIn(), outletReport.getTimeOut());
+
+                            if (lastSync.length() == 0) {
+                                lastSync = outletReport.getTimeOut();
+                            } else {
+                                if (SDUtil.compareDate(outletReport.getTimeOut(), lastSync, "yyyy/MM/dd HH:mm:ss") > 0) {
+                                    lastSync = outletReport.getTimeOut();
+                                }
+                            }
                         }
                     }
                     sellerPerformanceBO.setActual(actual + "");
@@ -137,6 +157,12 @@ public class SellerPerformanceReportFragment extends IvyBaseFragment {
                     sellerPerformanceBO.setDeviatedCall(deviateCall);
                     sellerPerformanceBO.setActualCall(actualCall);
                     sellerPerformanceBO.setProductiveCall(productiveCall);
+
+                    sellerPerformanceBO.setTimeSpent(TimeUnit.MILLISECONDS.toHours(duration) + ":" +
+                            TimeUnit.MILLISECONDS.toMinutes(duration) + ":" +
+                            TimeUnit.MILLISECONDS.toSeconds(duration));
+
+                    sellerPerformanceBO.setLastSync(lastSync);
                     int totalCall = plannedCall + deviateCall;
                     vistPer = (actualCall / totalCall) * 100;
                     sellerPerformanceBO.setVisitPer(bmodel.formatPercent(vistPer));
@@ -170,5 +196,59 @@ public class SellerPerformanceReportFragment extends IvyBaseFragment {
 
     }
 
+    private long getDiffDuration(String startDate, String endData) {
+        long diffDuration = 0;
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+        try {
+            Date d1 = format.parse(startDate);
+            Date d2 = format.parse(endData);
+            diffDuration = d2.getTime() - d1.getTime();
+        } catch (ParseException e) {
+            Commons.printException(e);
+        }
+        return diffDuration;
+    }
+
+    public class PerformanceAdapter extends RecyclerView.Adapter<PerformanceAdapter.MyViewHolder> {
+
+        private ArrayList<SellerPerformanceBO> performList;
+
+        public class MyViewHolder extends RecyclerView.ViewHolder {
+            public Button btnFilter;
+
+            public MyViewHolder(View view) {
+                super(view);
+                btnFilter = (Button) view.findViewById(R.id.btn_filter);
+                btnFilter.setTypeface(bmodel.configurationMasterHelper.getFontBaloobhai(ConfigurationMasterHelper.FontType.REGULAR));
+            }
+        }
+
+        public PerformanceAdapter(ArrayList<SellerPerformanceBO> performList) {
+            this.performList = performList;
+        }
+
+        @Override
+        public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View itemView = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.filter_rv_item, parent, false);
+
+            return new MyViewHolder(itemView);
+        }
+
+        @Override
+        public void onBindViewHolder(MyViewHolder holder, final int position) {
+            final SellerPerformanceBO performanceBO = performList.get(position);
+
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return performList.size();
+        }
+    }
 
 }
+
+
