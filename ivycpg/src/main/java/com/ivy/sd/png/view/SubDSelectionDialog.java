25ec735241792dd192d605cd3ab2745b3a468075
mansoor <mansoor.k@ivymobility.com>
2018-03-30 12:41:55 +0530

    Subd Selection Dialog added in Retaieler Visit Screen

--- ivycpg/src/main/java/com/ivy/sd/png/view/SubDSelectionDialog.java
+++ ivycpg/src/main/java/com/ivy/sd/png/view/SubDSelectionDialog.java
@@ -1,6 +1,7 @@
 package com.ivy.sd.png.view;
 
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -10,24 +11,32 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.view.Window;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CheckedTextView;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.LinearLayout.LayoutParams;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import com.ivy.lib.DialogFragment;
 import com.ivy.sd.png.asean.view.R;
+import com.ivy.sd.png.bo.ExpenseSheetBO;
 import com.ivy.sd.png.bo.RetailerMasterBO;
 import com.ivy.sd.png.bo.StandardListBO;
 import com.ivy.sd.png.model.BusinessModel;
+import com.ivy.sd.png.provider.ConfigurationMasterHelper;
 import com.ivy.sd.png.util.Commons;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Vector;
 
 public class SubDSelectionDialog extends DialogFragment {
 
@@ -35,6 +44,9 @@ public class SubDSelectionDialog extends DialogFragment {
     View v;
     private ListView mCountLV;
     private TextView mTitleTV;
+    Context context;
+    SubIdSelectionListner subIdSelectionListner;
+    int mSelectedSubId = -1;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -52,15 +64,94 @@ public class SubDSelectionDialog extends DialogFragment {
         bmodel = (BusinessModel) getActivity().getApplicationContext();
         bmodel.setContext(getActivity());
 
+        mSelectedSubId = getArguments().getInt("subDId");
+        Commons.print("mSelectedSubId, " + "" + mSelectedSubId);
 
-        mTitleTV =  v.findViewById(R.id.title);
-        mCountLV =  v.findViewById(R.id.lvSubd);
+        mTitleTV = v.findViewById(R.id.title);
+        mCountLV = v.findViewById(R.id.lvSubd);
 
-        ArrayAdapter<RetailerMasterBO> adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_single_choice, bmodel.getSubDMaster());
-        mCountLV.setAdapter(adapter);
+        mTitleTV.setTypeface(bmodel.configurationMasterHelper.getFontBaloobhai(ConfigurationMasterHelper.FontType.REGULAR));
+        Collections.sort(bmodel.getSubDMaster(), RetailerMasterBO.RetailerIsTodayComparator);
+        mCountLV.setAdapter(new MyAdapter(bmodel.getSubDMaster()));
         mCountLV.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
 
+        if (mSelectedSubId != -1) {
+            int mSelectedPostion = -1;
+            for (int position = 0; position < bmodel.getSubDMaster().size(); position++) {
+                if (mSelectedSubId == bmodel.getSubDMaster().get(position).getSubdId())
+                    mSelectedPostion = position;
+            }
+            if (mSelectedPostion != -1)
+                mCountLV.setItemChecked(mSelectedPostion, true);
+        }
+
+
+        mCountLV.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                subIdSelectionListner.onSubIdSelected(bmodel.getSubDMaster().get(position).getSubdId());
+                dismiss();
+            }
+        });
+
         return v;
+    }
+
+    class ViewHolder {
+        CheckedTextView tvRetailerName;
+        RetailerMasterBO retaierBo;
+    }
+
+    private class MyAdapter extends ArrayAdapter<RetailerMasterBO> {
+        private Vector<RetailerMasterBO> items;
+
+        public MyAdapter(Vector<RetailerMasterBO> items) {
+            super(getActivity(), android.R.layout.simple_list_item_single_choice);
+            this.items = items;
+        }
+
+        public RetailerMasterBO getItem(int position) {
+            return items.get(position);
+        }
+
+        public long getItemId(int position) {
+            return position;
+        }
+
+        public int getCount() {
+            return items.size();
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            final ViewHolder holder;
+
+            if (convertView == null) {
+
+                holder = new ViewHolder();
+
+                LayoutInflater inflater = LayoutInflater.from(getActivity().getBaseContext());
+
+                convertView = inflater.inflate(android.R.layout.simple_list_item_single_choice, null);
+
+                holder.tvRetailerName = convertView.findViewById(android.R.id.text1);
+                holder.tvRetailerName.setTypeface(bmodel.configurationMasterHelper.getFontRoboto(ConfigurationMasterHelper.FontType.LIGHT));
+
+                convertView.setTag(holder);
+
+            } else {
+                holder = (ViewHolder) convertView.getTag();
+            }
+
+            holder.retaierBo = items.get(position);
+            holder.tvRetailerName.setText(holder.retaierBo.getRetailerName());
+            if (holder.retaierBo.getIsToday() == 1)
+                holder.tvRetailerName.setTextColor(getResources().getColor(R.color.colorAccent));
+            else
+                holder.tvRetailerName.setTextColor(getResources().getColor(R.color.colorPrimaryRed));
+
+            return convertView;
+        }
     }
 
 
@@ -86,23 +177,11 @@ public class SubDSelectionDialog extends DialogFragment {
 
     }
 
-
-
-    private DialogInterface.OnDismissListener onDismissListener;
-
-    public void setOnDismissListener(DialogInterface.OnDismissListener onDismissListener) {
-        this.onDismissListener = onDismissListener;
+    public interface SubIdSelectionListner {
+        void onSubIdSelected(int SubID);
     }
 
-    @Override
-    public void onDismiss(DialogInterface dialog) {
-        super.onDismiss(dialog);
-        if (onDismissListener != null) {
-            onDismissListener.onDismiss(dialog);
-        }
+    public void setSubIdSelectionInterface(VisitFragment visitFragment) {
+        this.subIdSelectionListner = visitFragment;
     }
-
-
-
-
 }
