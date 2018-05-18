f9a0027072d3862c177e33e094ba95aa25e0bc01
sajena <sajen.a@ivymobility.com>
2018-05-18 11:46:30 +0530

    Wrong file comitted

--- ivycpg/src/main/java/com/ivy/sd/png/model/ApplicationConfigs.java
+++ ivycpg/src/main/java/com/ivy/sd/png/model/ApplicationConfigs.java
@@ -3,9 +3,9 @@ package com.ivy.sd.png.model;
 public interface
 ApplicationConfigs {
 
-    String DB_NAME = "ivycpg_may14";
+    String DB_NAME = "ivycpg_v18";
 
-    boolean withActivation = false;
+    boolean withActivation = true;
 
     boolean checkUTCTime = true;
     String LANGUAGE = "en";
