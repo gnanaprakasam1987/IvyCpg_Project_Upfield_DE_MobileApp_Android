0d135d4965e450f16360b6d00b88dcfdd1036f4d
anbarasan.g <AnbArAsAn.G>
2018-05-31 13:31:22 +0530

    Merge remote-tracking branch 'remotes/origin/master' into CPG130_Bislari_Anbu
    Conflicts Resolved:

--- ivycpg/src/main/java/com/ivy/sd/png/model/ApplicationConfigs.java
+++ ivycpg/src/main/java/com/ivy/sd/png/model/ApplicationConfigs.java
@@ -3,9 +3,9 @@ package com.ivy.sd.png.model;
 public interface
 ApplicationConfigs {
 
-    String DB_NAME = "ivycpg_v18";
+    String DB_NAME = "ivycpg_v19";
 
-    boolean withActivation = false;
+    boolean withActivation = true;
 
     boolean checkUTCTime = true;
     String LANGUAGE = "en";
