a3443dd2a0d325a7fda700483e7ccf7283e145dc
kaviyarasan.p <kaviyarasan.p@ivymobility.com>
2018-06-26 13:54:01 +0530

    BaseFragment base methods updated

--- ivycpg/src/main/java/com/ivy/core/base/view/BaseFragment.java
+++ ivycpg/src/main/java/com/ivy/core/base/view/BaseFragment.java
@@ -7,10 +7,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.ivy.core.base.presenter.BasePresenter;
+import com.ivy.sd.png.provider.ConfigurationMasterHelper;
+
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
 
-public abstract class BaseFragment extends Fragment {
+public abstract class BaseFragment extends Fragment implements BaseIvyView {
 
 
     private Unbinder mUnBinder;
@@ -27,13 +30,104 @@ public abstract class BaseFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         mUnBinder = ButterKnife.bind(this, view);
         initVariables(view);
+
+
+        initializeDi();
+
+        getMessageFromAliens();
+
+        setUpViews();
+
     }
 
+    private BasePresenter mBasePresenter;
+
+    public void setBasePresenter(BasePresenter presenter) {
+        mBasePresenter = presenter;
+    }
+
+    public abstract void initializeDi();
 
     protected abstract int setContentViewLayout();
 
     public abstract void initVariables(View view);
 
+    /**
+     * Abstract method which can be used to get the data
+     * via intent for other activities
+     */
+    protected abstract void getMessageFromAliens();
+
+    /**
+     * Set up the views.
+     */
+    protected abstract void setUpViews();
+
+
+
+    @Override
+    public void showLoading(int strinRes) {
+        ((BaseActivity) getActivity()).showLoading(strinRes);
+    }
+
+    @Override
+    public void showLoading(String message) {
+        ((BaseActivity) getActivity()).showLoading(message);
+    }
+
+    @Override
+    public void showLoading() {
+        ((BaseActivity) getActivity()).showLoading();
+    }
+
+    @Override
+    public void hideLoading() {
+        ((BaseActivity) getActivity()).hideLoading();
+    }
+
+    @Override
+    public void hideKeyboard() {
+        ((BaseActivity) getActivity()).hideKeyboard();
+    }
+
+    @Override
+    public void onError(int resId) {
+        ((BaseActivity) getActivity()).onError(resId);
+    }
+
+    @Override
+    public void onError(String message) {
+        ((BaseActivity) getActivity()).onError(message);
+    }
+
+    @Override
+    public void showMessage(int resId) {
+        ((BaseActivity) getActivity()).showMessage(resId);
+    }
+
+    @Override
+    public void showMessage(String message) {
+        ((BaseActivity) getActivity()).showMessage(message);
+    }
+
+    @Override
+    public boolean isNetworkConnected() {
+        return ((BaseActivity) getActivity()).isNetworkConnected();
+    }
+
+    /**
+     * Set the unBinder object from butter knife so that the unbinding can be
+     * taken care from the base activity on destroy
+     *
+     * @param unBinder unBinder of the ButterKnife
+     */
+    public void setUnBinder(Unbinder unBinder) {
+        mUnBinder = unBinder;
+    }
+
+    public ConfigurationMasterHelper getConfigurationMasterHelper() {
+        return ((BaseActivity) getActivity()).getConfigurationMasterHelper();
+    }
 
     @Override
     public void onDestroyView() {
