c1d1e628f9af29ac70c91da23570e503268d7278
velraj <velraj460@gmail.com>
2018-09-26 11:33:48 +0530

    IVYCPG-1276 scheme isue move to 133 commit

--- ivycpg/src/main/java/com/ivy/cpg/view/order/discount/DiscountHelper.java
+++ ivycpg/src/main/java/com/ivy/cpg/view/order/discount/DiscountHelper.java
@@ -967,6 +967,7 @@ public class DiscountHelper {
                                     if (!productIdList.contains(productBO.getProductID())) {
                                         productIdList.add(productBO.getProductID());
                                         i = i++;
+                                        boolean isFreeProductGiven = false;
                                         if (productBO.getOrderedPcsQty() > 0
                                                 || productBO.getOrderedCaseQty() > 0
                                                 || productBO.getOrderedOuterQty() > 0) {
@@ -1123,15 +1124,19 @@ public class DiscountHelper {
                                                 totalSchemeDiscountValue += totalPercentageDiscount;
                                             } else if (schemeBO
                                                     .isQuantityTypeSelected()) {
-                                                orderHelper.updateSchemeFreeProduct(schemeBO, productBO);
+                                                if(!isFreeProductGiven) {
+                                                    orderHelper.updateSchemeFreeProduct(schemeBO, productBO);
+                                                    isFreeProductGiven=true;
+                                                }
                                                 break;
                                             }
                                         } else {
                                             if (schemeBO.isQuantityTypeSelected()) {
                                                 // if  Accumulation scheme's buy product not available, free product set in First order product object
-                                                if (i == schemeProductList.size() && !isBuyProductAvailable) {
+                                                if (!isFreeProductGiven && !isBuyProductAvailable) {
                                                     ProductMasterBO firstProductBO = mOrderedList.get(0);
                                                     orderHelper.updateSchemeFreeProduct(schemeBO, firstProductBO);
+                                                    isFreeProductGiven=true;
                                                 }
                                             }
                                         }
