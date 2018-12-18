3d3ab2b377a3b0718575cc316b5d83f65e2ea98c
Kaviyarasan P <kaviyarasan.p@ivymobility.com>
2018-12-18 12:22:56 +0000

    Merge branch 'CPG130_IssueFixes_Anand' into 'CPG130_Release'
    JIRA IVYCPG-2322 Sync Data download issue fixed
    
    See merge request IvyProjects/IvyCpg_Product_MobileApp_Android!785
    
    (cherry picked from commit 4bd7d9a7a2b997c63b679cf12d9935cb352c5ea9)
    
    0b366fcc JIRA IVYCPG-2322 Sync Data download issue fixed

--- ivycpg/src/main/java/com/ivy/sd/png/view/SynchronizationFragment.java
+++ ivycpg/src/main/java/com/ivy/sd/png/view/SynchronizationFragment.java
@@ -1452,6 +1452,8 @@ public class SynchronizationFragment extends IvyBaseFragment
                 } else {
                     Toast.makeText(getActivity(), getResources().getString(R.string.data_not_downloaded), Toast.LENGTH_SHORT).show();
                 }
+                if (alertDialog != null)
+                    alertDialog.dismiss();
             }
 
         }
