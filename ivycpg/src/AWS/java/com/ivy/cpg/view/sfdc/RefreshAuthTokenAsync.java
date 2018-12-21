e69571174bde27e427bbf4dff9f9b8edb6e113c7
kaviyarasan.p <kaviyarasan.p@ivymobility.com>
2018-12-21 15:32:58 +0530

    Aws source sets moved inside java

---
+++ ivycpg/src/AWS/java/com/ivy/cpg/view/sfdc/RefreshAuthTokenAsync.java
@@ -0,0 +1,19 @@
+package com.ivy.cpg.view.sfdc;
+
+import android.content.Context;
+import android.os.AsyncTask;
+
+import com.ivy.sd.png.provider.SynchronizationHelper;
+
+public class RefreshAuthTokenAsync extends AsyncTask<String, String, String> {
+
+
+    public RefreshAuthTokenAsync(Context context, SynchronizationHelper.VolleyResponseCallbackInterface volleySFDCnormalDownload) {
+    }
+
+
+    @Override
+    protected String doInBackground(String... strings) {
+        return null;
+    }
+}
