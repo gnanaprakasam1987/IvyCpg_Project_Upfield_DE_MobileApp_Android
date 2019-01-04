1f1d8a7deea65a7c40771fe535f1599e489c951a
mansoor <mansoor.k@ivymobility.com>
2019-01-04 15:58:01 +0530

    Free SIh master impact handled in van load nd unload screens also in eod report and current stock report

--- ivycpg/src/main/java/com/ivy/sd/png/model/ApplicationConfigs.java
+++ ivycpg/src/main/java/com/ivy/sd/png/model/ApplicationConfigs.java
@@ -5,7 +5,7 @@ import com.ivy.sd.png.asean.view.BuildConfig;
 public interface
 ApplicationConfigs {
 
-    String DB_NAME = "ivycpg_v19";
+    String DB_NAME = "ivycpg_v1955";
 
     boolean withActivation = BuildConfig.ACTIVATION;
 
