6e5dd0d3cc779a4d0c79ca8b54158e8f747cde05
kaviyarasan.p <kaviyarasan.p@ivymobility.com>
2019-02-15 15:13:51 +0530

    String utils test cases updated

--- ivycpg/src/test/java/com/ivy/utils/StringUtilsTest.java
+++ ivycpg/src/test/java/com/ivy/utils/StringUtilsTest.java
@@ -35,4 +35,34 @@ public class StringUtilsTest {
     public void testIsNotEmptyString(){
         Assert.assertFalse(StringUtils.isEmptyString("Hello"));
     }
+
+    @Test
+    public void testValidRegexPositive(){
+        Assert.assertTrue(StringUtils.validRegex("^\\s*(?:\\+?(\\d{1,3}))?([-. (]*(\\d{3})[-. )]*)?((\\d{3})[-. ]*(\\d{2,4})(?:[-.x ]*(\\d+))?)\\s*$","+79261234567"));
+    }
+
+    @Test
+    public void testValidRegexNegative(){
+        Assert.assertFalse(StringUtils.validRegex("^\\s*(?:\\+?(\\d{1,3}))?([-. (]*(\\d{3})[-. )]*)?((\\d{3})[-. ]*(\\d{2,4})(?:[-.x ]*(\\d+))?)\\s*$","abcdef"));
+    }
+
+    @Test
+    public void testValidRegexEmpty(){
+        Assert.assertTrue(StringUtils.validRegex("","abcdef"));
+    }
+
+    @Test
+    public void testIsValidEmailSuccess(){
+        Assert.assertTrue(StringUtils.isValidEmail("abc@def.com"));
+    }
+
+    @Test
+    public void testIsValidEmailFailure(){
+        Assert.assertFalse(StringUtils.isValidEmail("abc@def.def..com"));
+    }
+
+    @Test
+    public void testQT(){
+        Assert.assertEquals("'abcdef'",StringUtils.QT("abcdef"));
+    }
 }
