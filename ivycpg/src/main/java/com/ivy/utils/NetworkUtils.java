4b4c1b408f0bc2bf9bc6152456075157e7fca185
kaviyarasan.p <kaviyarasan.p@ivymobility.com>
2019-03-06 15:09:50 +0530

    Newtwork connectivity validation updated

--- ivycpg/src/main/java/com/ivy/utils/NetworkUtils.java
+++ ivycpg/src/main/java/com/ivy/utils/NetworkUtils.java
@@ -6,6 +6,10 @@ import android.net.NetworkInfo;
 
 import com.amazonaws.com.google.gson.Gson;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 
 /**
  * All Network Level utilities are defined here
@@ -27,9 +31,23 @@ public class NetworkUtils {
                 (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
         assert cm != null;
         NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
-        return activeNetwork != null && activeNetwork.isConnectedOrConnecting();
+        return activeNetwork != null && activeNetwork.isConnectedOrConnecting() && isInternetWorking();
     }
 
+
+    public static boolean isInternetWorking() {
+        boolean success = false;
+        try {
+            URL url = new URL("https://google.com");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setConnectTimeout(10000);
+            connection.connect();
+            success = connection.getResponseCode() == 200;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return success;
+    }
 
 
 
