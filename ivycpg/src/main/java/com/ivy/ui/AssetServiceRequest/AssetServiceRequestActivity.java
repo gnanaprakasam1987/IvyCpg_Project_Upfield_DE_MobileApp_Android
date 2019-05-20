844ba01e81c72bcfefa8555689f16616caa7d67b
rajkumar.s <rajkumar.s@ivymobility.com>
2019-05-20 16:56:36 +0530

    local commit

--- ivycpg/src/main/java/com/ivy/ui/AssetServiceRequest/AssetServiceRequestActivity.java
+++ ivycpg/src/main/java/com/ivy/ui/AssetServiceRequest/AssetServiceRequestActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RadioButton;
@@ -25,6 +27,15 @@ public class AssetServiceRequestActivity extends AppCompatActivity {
     }
 
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
 
     class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyViewHolder> {
 
---
+++ ivycpg/src/main/java/com/ivy/ui/AssetServiceRequest/NewAssetServiceRequest.java
@@ -0,0 +1,15 @@
+package com.ivy.ui.AssetServiceRequest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ivy.sd.png.asean.view.R;
+
+public class NewAssetServiceRequest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_asset_service_request);
+    }
+}
