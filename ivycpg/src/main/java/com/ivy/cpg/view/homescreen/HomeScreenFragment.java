7b0764edf0654a89b72ceaf7d46eddc95beeca3b
Anand Asir <anandasir.v@ivymobility.com>
2019-06-10 13:30:41 +0000

    Merge branch 'CPG141_SFDCIntegration_Anand' into 'CPG141_Release'
    Logout button renamed as Close button
    
    See merge request IvyProjects/IvyCpg_Product_MobileApp_Android!1570
    
    (cherry picked from commit 5313417cd2d5ad0515589cd794f1c65f24395aa9)
    
    9c9f3048 Logout button renamed as Close button

--- ivycpg/src/main/java/com/ivy/cpg/view/homescreen/HomeScreenFragment.java
+++ ivycpg/src/main/java/com/ivy/cpg/view/homescreen/HomeScreenFragment.java
@@ -88,6 +88,7 @@ import com.ivy.cpg.view.van.stockproposal.StockProposalFragment;
 import com.ivy.cpg.view.webview.WebViewActivity;
 import com.ivy.maplib.PlanningMapFragment;
 import com.ivy.sd.camera.CameraActivity;
+import com.ivy.sd.png.asean.view.BuildConfig;
 import com.ivy.sd.png.asean.view.R;
 import com.ivy.sd.png.bo.ChannelBO;
 import com.ivy.sd.png.bo.ConfigureBO;
@@ -307,6 +308,10 @@ public class HomeScreenFragment extends IvyBaseFragment implements VisitFragment
                 }
             }
         });
+
+        TextView txtLogOut = (TextView)view.findViewById(R.id.txtLogOut);
+        if (!BuildConfig.FLAVOR.equalsIgnoreCase("aws"))
+            txtLogOut.setText(getResources().getString(R.string.close));
 
         LinearLayout ll_logout = view.findViewById(R.id.ll_logout);
         ll_logout.setOnClickListener(new OnClickListener() {
@@ -594,7 +599,8 @@ public class HomeScreenFragment extends IvyBaseFragment implements VisitFragment
             case 0:
                 AlertDialog.Builder builder = new AlertDialog.Builder(getActivity())
                         .setTitle(
-                                getResources().getString(R.string.do_u_want_logout))
+                                BuildConfig.FLAVOR.equalsIgnoreCase("aws") ? getResources().getString(R.string.do_u_want_logout) :
+                        getResources().getString(R.string.do_u_want_close))
                         .setCancelable(false)
                         .setPositiveButton(getResources().getString(R.string.ok),
                                 new DialogInterface.OnClickListener() {
