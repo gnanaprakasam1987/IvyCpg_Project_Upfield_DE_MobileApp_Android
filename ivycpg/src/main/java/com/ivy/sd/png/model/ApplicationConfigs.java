7cb2ed60136c340a14a9509cd6fefee80d230ca1
abbas.a <abbas.a@ivymobility.com>
2019-07-22 19:08:53 +0530

    New DB version added

--- ivycpg/src/main/java/com/ivy/sd/png/model/ApplicationConfigs.java
+++ ivycpg/src/main/java/com/ivy/sd/png/model/ApplicationConfigs.java
@@ -5,7 +5,7 @@ import com.ivy.sd.png.asean.view.BuildConfig;
 public interface
 ApplicationConfigs {
 
-    String DB_NAME = "ivycpg_v20";
+    String DB_NAME = "ivycpg_v21";
 
     boolean withActivation = BuildConfig.ACTIVATION;
 
